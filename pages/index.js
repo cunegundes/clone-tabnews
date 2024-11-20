9782ddc1d72bafb908280549d0e47b31e1c871be
Felippi Cunegundes Laender <felippilaender@gmail.com>
2024-11-20 17:37:07 +0000

    Modificando página

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Olá, Jaqueline! Eu te amo e você me ama, dá uma risadinha =)</h1>;
+    return <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha =)</h1>;
 }
 export default Home;
