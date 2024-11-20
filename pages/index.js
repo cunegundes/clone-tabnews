c907664e810f703c5d4549804a787b2b6403b92a
Felippi Cunegundes Laender <felippilaender@gmail.com>
2024-11-20 22:00:46 +0000

    Declarando meu amor para minha filha

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 function Home() {
-    return <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>;
-}
+    return (
+        <>
+        <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>;
+        <h2>Nena, minha pequena, você é a estrela da minha vida. 🌟</h2>
+        </>
+    )
+   
+};
 export default Home;
