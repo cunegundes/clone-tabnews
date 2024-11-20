86e52087f98e30768db7453ff1bf6fa962f2f944
Felippi Cunegundes Laender <felippilaender@gmail.com>
2024-11-20 22:02:02 +0000

    Correcao de texto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
     return (
         <>
-        <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>;
-        <h2>Nena, minha pequena, você é a estrela da minha vida. 🌟</h2>
+        <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>
+        <h2>Nena, minha pequena. Você é a estrela da minha vida. 🌟</h2>
         </>
     )
    
