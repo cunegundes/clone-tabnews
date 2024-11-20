31c4b5a1b308ad043a135336d0c1a94b06cdb2e3
Felippi Cunegundes Laender <felippilaender@gmail.com>
2024-11-20 17:51:52 +0000

    adicionando emoji

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha =)</h1>;
+    return <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>;
 }
 export default Home;
