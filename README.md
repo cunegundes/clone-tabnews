fd6025c6e2c27fb55e0fc12702c3fc872044e6f3
Felippi Cunegundes Laender <felippilaender@gmail.com>
2024-11-22 12:51:07 +0000

    Alterando formatação dos arquivos com o Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do projeto tabnews para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 function Home() {
-    return (
-        <>
-        <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>
-        <h2>Nena, minha pequena. Você é a estrela da minha vida. 🌟</h2>
-        </>
-    )
-   
-};
+  return (
+    <>
+      <h1>Olá, Jaqueline! Eu te amo e se você me ama, dá uma risadinha 😊</h1>
+      <h2>Nena, minha pequena. Você é a estrela da minha vida. 🌟</h2>
+    </>
+  );
+}
 export default Home;
